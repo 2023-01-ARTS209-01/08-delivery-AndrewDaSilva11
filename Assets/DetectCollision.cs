2d36b1993683ce48c81dc142ef08943d2b1dfcdc
AndrewDaSilva11 <113034792+AndrewDaSilva11@users.noreply.github.com>
2023-03-17 12:35:49 -0400

    Driver

---
+++ Assets/DetectCollision.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    
+private void OnCollisionEnter2D(Collision2D collision)
+{
+
+Debug.Log("We hit something! - " + collision.gameObject.name);
+
+}
+
+private void OnTriggerEnter2D(Collider2D trigger)
+{
+    Debug.Log("We went though a TRIGGER! - " + trigger.gameObject.name);
+}
+
+}
