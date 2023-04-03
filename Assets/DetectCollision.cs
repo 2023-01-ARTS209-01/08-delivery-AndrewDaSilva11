f8e233cc42174e2e456758259cb5de4f1f59ebc9
AndrewDaSilva11 <113034792+AndrewDaSilva11@users.noreply.github.com>
2023-04-02 21:57:33 -0400

    delovey

--- Assets/DetectCollision.cs
+++ Assets/DetectCollision.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
+private bool hasPackage = false;
+
+ SpriteRenderer spriteRenderer;
+[SerializeField] Color hasPackageColor;
+[SerializeField] Color doesNotHavePackageColor;
+
+private void Start()
+{
+    spriteRenderer = GetComponent<SpriteRenderer>();
+}
     
 private void OnCollisionEnter2D(Collision2D collision)
 {
@@ -14,7 +24,21 @@ Debug.Log("We hit something! - " + collision.gameObject.name);
 
 private void OnTriggerEnter2D(Collider2D trigger)
 {
-    Debug.Log("We went though a TRIGGER! - " + trigger.gameObject.name);
+    //Debug.Log("We went though a TRIGGER! - " + trigger.gameObject.name);
+
+    if (trigger.gameObject.CompareTag("Package") && !hasPackage)
+    {
+        Debug.Log("Picked up package!");
+        hasPackage = true;
+        spriteRenderer.color = hasPackageColor;
+        Destroy (trigger.gameObject, 0.5f);
+    }
+    if (trigger.gameObject.CompareTag("Customer") && hasPackage)
+    {
+     Debug.Log("Delivered the package!!");
+     spriteRenderer.color = doesNotHavePackageColor;
+     hasPackage = false;
+    }
 }
 
 }
